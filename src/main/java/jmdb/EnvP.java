51a38881481fb8301f0f202162c7f03fcb6d2789
Cloudee Huang <cloudeecn@gmail.com>
2014-03-31 11:50:50 +0800

    license

--- src/main/java/jmdb/EnvP.java
+++ src/main/java/jmdb/EnvP.java
@@ -1,3 +1,21 @@
+/**
+ *  Copyright 2014-2014 Yuxuan Huang. All rights reserved.
+ *
+ * This file is part of jmdb
+ *
+ * jmdb is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * jmdb is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with jmdb. If not, see <http://www.gnu.org/licenses/>.
+ */
 package jmdb;
 
 import java.io.Closeable;
